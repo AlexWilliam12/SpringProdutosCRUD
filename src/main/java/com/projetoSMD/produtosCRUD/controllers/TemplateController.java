702f27be83fb0662a45e538e2fdbacda184f55a2
Alexandre <william20alex01@gmail.com>
2023-06-02 18:11:21 -0300

    Alteração

--- src/main/java/com/projetoSMD/produtosCRUD/controllers/TemplateController.java
+++ src/main/java/com/projetoSMD/produtosCRUD/controllers/TemplateController.java
@@ -1,12 +1,12 @@
 package com.projetoSMD.produtosCRUD.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TemplateController {
 
-	@GetMapping("/produtos")
+	@RequestMapping("/produtos")
 	public String produtosTemplate() {
 		return "produtos";
 	}
